9f04c16999e82e7589c0bbbcf3f3ae9f1c7e2d97
weifei0228 <weifei0228@gmail.com>
2018-01-11 13:30:12 +0800

    changed app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flaskr.factory import create_app
-from flaskr.blueprints.flaskr import init_db
+#from flaskr.blueprints.flaskr import init_db
 
 app=create_app()
 #init_db()
 with app.app_context():
-	init_db()
-if __name__=="__main__":
-    #app.run(host='0.0.0.0', port=8080)
 	
-	app.run()
+	if __name__=="__main__":
+		#app.run(host='0.0.0.0', port=8080)
+	
+		app.run()
 	
